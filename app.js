2dca7f6c6cf7f77682f1575afe0e9cdd8b9fa44f
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-04-07 19:58:37 -0500

    got search functionality working, prepends images to the image div now, still need to isolate urls for animated and fixed and toggle between the two

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
 //key WLXZ77HfPkwbMGs80cGMtM6qj1hK88MD
 
-var topics = ["Ozzy Osbourne", "E.T.", "Megaman", "Pulp", "The Karate Kid", "Slay the Spire"];
+var topics = [
+  "Ozzy Osbourne",
+  "ET",
+  "Megaman",
+  "Pulp",
+  "The Karate Kid",
+  "Slay the Spire"
+];
+var gifUrl;
 
-
-
-function makeButton (nameOfButton) {
-    return `
+function makeButton(nameOfButton) {
+  return `
     <button type="button" class="btn btn-primary"  id="button">
     ${nameOfButton}
     </button>
-    `
-};
+    `;
+}
 
-$(document).on("click", "#submit", function(){
-    var newButton = $("#userInput").val();
-    event.preventDefault();
-    $(".buttonFrame").append(makeButton(newButton));
-
+$(document).on("click", "#submit", function() {
+  var newButton = $("#userInput").val();
+  event.preventDefault();
+  $(".buttonFrame").append(makeButton(newButton));
 });
 
 $(".buttonFrame").html(topics.map(makeButton));
 
+function displayGif(obj) {
+  const gifToDisplay = obj.images.fixed_height.url;
+  return `
+    <img src="${gifToDisplay}"/>
+    `;
+}
 
-$(document).on("click", "#button", function(){
-    var gifSearch = $(this).text();
-    console.log(gifSearch);
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+gifSearch+"&api_key=WLXZ77HfPkwbMGs80cGMtM6qj1hK88MD&limit=10"
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response)
-    })
-})
+$(document).on("click", "#button", function() {
+  var gifSearch = $(this).text();
+  var queryURL =
+    "http://api.giphy.com/v1/gifs/search?q=" +
+    gifSearch +
+    "&api_key=WLXZ77HfPkwbMGs80cGMtM6qj1hK88MD&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var gifArray = response.data;
+    console.log(response);
+    console.log(gifArray);
+    $("#gifFrame").prepend(response.data.map(displayGif));
+  });
+});
