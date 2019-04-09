13278d49f671d96b1c4065786ae3e85d7c4c8720
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-04-08 22:52:45 -0500

    can now click the gif and "pause" the gif

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //key WLXZ77HfPkwbMGs80cGMtM6qj1hK88MD
-
 var topics = [
   "Ozzy Osbourne",
   "ET",
@@ -9,7 +8,7 @@ var topics = [
   "Slay the Spire"
 ];
 var gifUrl;
-
+//returns a button with the text passed through as the name
 function makeButton(nameOfButton) {
   return `
     <button type="button" class="btn btn-primary"  id="button">
@@ -17,22 +16,29 @@ function makeButton(nameOfButton) {
     </button>
     `;
 }
-
+//after clicking the submit button, a new button with the 
+//users text as the name of the button, is displayed on the page
 $(document).on("click", "#submit", function() {
   var newButton = $("#userInput").val();
   event.preventDefault();
   $(".buttonFrame").append(makeButton(newButton));
 });
-
+//goes through each entry in the topics array and makes a button for each entry
 $(".buttonFrame").html(topics.map(makeButton));
-
+//returns a img tag with the source set to the giphy query results,
+//also holds the still source and the data-state attribute
 function displayGif(obj) {
   const gifToDisplay = obj.images.fixed_height.url;
+  const stillGif = obj.images.fixed_height_still.url;
   return `
-    <img src="${gifToDisplay}"/>
+    <img class="gif" src="${gifToDisplay}"
+    data-still="${stillGif}"
+    data-animated="${gifToDisplay}"
+    data-state="animated"
+    />
     `;
 }
-
+//upon clicking the new or old button the giphy database is queried, and the gif frame will display 10 new gifs based on the query results
 $(document).on("click", "#button", function() {
   var gifSearch = $(this).text();
   var queryURL =
@@ -48,4 +54,15 @@ $(document).on("click", "#button", function() {
     console.log(gifArray);
     $("#gifFrame").prepend(response.data.map(displayGif));
   });
+});
+
+$(document).on("click", ".gif", function() {
+  var state = $(".gif").attr("data-state");
+  if (state === "animated") {
+    $(this).attr("data-state", "still")
+    $(this).attr("src", $(this).attr("data-still"))
+  } else {
+    $(this).attr("data-state", "animated")
+    $(this).attr("src", $(this).attr("data-animated"))
+  }
 });
